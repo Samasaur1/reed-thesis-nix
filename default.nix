8ab6f6ecaf01c5dfc4e374c4dd83f7ce2472b118
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-09-04 15:45:34 -0700

    initial commit

---
+++ default.nix
@@ -0,0 +1,30 @@
+{ lib, stdenvNoCC, writeShellScript, ... }:
+
+stdenvNoCC.mkDerivation {
+  pname = "reedthesis";
+  version = "1.0.0";
+
+  outputs = [ "tex" ];
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    (writeShellScript "force-tex-output.sh" ''
+     out="''${tex-}"
+     '')
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    path="$tex/tex/latex/reedthesis"
+
+    cp reedthesis.cls "$path/"
+
+    runHook postInstall
+  '';
+  
+  meta = with lib; {
+    platforms = platforms.all;
+  };
+}
